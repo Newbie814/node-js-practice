487f548458da752e7e6b334088d85c06a3f35b23
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-20 16:42:49 -0400

    fixed cors error from interacting with front end server program

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
 
+const locationRoutes = require('./routes/location');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {});
+
 app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.use((req, res, next) => {
-  let userName = req.body.username || 'unknown';
+app.use(locationRoutes);
 
-  res.render('index', {
-    user: userName,
-  });
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   let userName = req.body.username || 'unknown';
+
+//   res.render('index', {
+//     user: userName,
+//   });
+// });
 
 app.listen(3000);
